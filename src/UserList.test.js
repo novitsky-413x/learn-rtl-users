a7755b06c0e024f0bce984902f14d5311c021808
Alexander Novitsky <novitsky.413x@gmail.com>
2025-03-24 19:00:10 +0300

    21. Getting Help with Query Functions

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -10,7 +10,12 @@ test('render one row per user', () => {
     render(<UserList users={users} />);
 
     // Find all the rows in the table
+    // // helps to pick the right selector, see link in jest console
+    // // screen.logTestingPlaygroundURL();
+    const rows = screen.getAllByRole('row');
+
     // Assertion: correct number of rows in the table
+    expect(rows).toHaveLength(2);
 });
 
 test('render the email and name of each user', () => {});
