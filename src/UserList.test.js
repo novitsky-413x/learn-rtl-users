97180998a67494868a8388e356b6ef27b2bb4008
Alexander Novitsky <novitsky.413x@gmail.com>
2025-03-24 19:50:49 +0300

    23. Another Query Function Fallback

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -7,12 +7,17 @@ test('render one row per user', () => {
         { name: 'John Doe', email: 'johndoe@example.com' },
         { name: 'Sam Smith', email: 'samsmith@example.com' },
     ];
+    // const { container } = render(<UserList users={users} />);
     render(<UserList users={users} />);
 
-    // Find all the rows in the table
     // // helps to pick the right selector, see link in jest console
     // // screen.logTestingPlaygroundURL();
+
+    // Find all the rows in the table
+    // eslint-disable-next-line
+    // const rows = container.querySelectorAll('tbody tr');
     const rows = within(screen.getByTestId('users')).getAllByRole('row');
+    // example: <tbody data-testid="users">{renderedUsers}</tbody>
 
     // Assertion: correct number of rows in the table
     expect(rows).toHaveLength(2);
