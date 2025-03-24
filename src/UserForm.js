f5c5ef183f8fcf24ccf19b92a05c9c3b27d802af
Alexander Novitsky <novitsky.413x@gmail.com>
2025-03-24 09:56:18 +0300

    8. Handling User Input

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
+
 function UserForm() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(name, email);
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>Name</label>
-                <input />
+                <input value={name} onChange={(e) => setName(e.target.value)} />
             </div>
             <div>
                 <label>Email</label>
-                <input />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <button>Add User</button>
         </form>
