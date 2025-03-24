f05a84f075a249e2f9e3ed7fdcb21e795ff36b1f
Alexander Novitsky <novitsky.413x@gmail.com>
2025-03-24 18:42:44 +0300

    20. Testing the UserList

---
+++ src/UserList.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import UserList from './UserList';
+
+test('render one row per user', () => {
+    // Render the component
+    const users = [
+        { name: 'John Doe', email: 'johndoe@example.com' },
+        { name: 'Sam Smith', email: 'samsmith@example.com' },
+    ];
+    render(<UserList users={users} />);
+
+    // Find all the rows in the table
+    // Assertion: correct number of rows in the table
+});
+
+test('render the email and name of each user', () => {});
